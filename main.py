81d7298bf8f8be4a7db91678dff7e642e6766e80
Ramses Budé <95089348+Rambam111@users.noreply.github.com>
2022-04-22 12:48:23 +0000

    Start van het spel, naam invullen en optie start spel

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to Python galgje")
+usertext = input("What is your name? ")
+print("Hello", usertext)
+
+print("What do you want to do?")
+usertext = input("Type "Play or Options or Instructions"")
+ if usertext = play
