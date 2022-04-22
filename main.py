2b617ba759ea669ce8b36cd6bcbeb7dd73ba70b2
Ramses Budé <95089348+Rambam111@users.noreply.github.com>
2022-04-22 13:41:58 +0000

    einde les instructies

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ print("-Options")
 
 inp = input("Enter a word:")
 if inp == "Start":
-  print("Alrtight, let's begin then!")
-elif inp == 2:
-    inp = "bar"
+  print("Alright, let's begin then!")
+elif inp == "Instructions":
+  print("Before the game starts, you can choose the options of the game. You can choose the length of the word you have to guess. Once you presses play, you have 8 tries to guess the right word, by choosing diffrent types of letters. If the letter is found in the word, it will be put on the dots. If the letter is not in the word, than the start of a picture will be drawn. If the picture is completed before you have finished the word than you lose. Have you guessed the word, than you win.")
 elif inp == 3:
     inp = "spam"
 else:
