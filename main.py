01ae177b600be4d3bc1618f531185f16089538d1
Ramses Budé <95089348+Rambam111@users.noreply.github.com>
2022-06-10 13:30:59 +0000

    Klaar met instructies, je kan nu doorspelen na de instructies.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-woorden = ["champion", "eagles", "promise", "stripes", "pavement", "motorcycle", "square", "mountain", "waterfall", "NIGGERS"]
+woorden = ["champion", "eagles", "promise", "stripes", "pavement", "motorcycle", "square", "mountain", "waterfall",]
 galg = [
   "---------",
   "|       |",
@@ -49,8 +49,9 @@ if inp == "Start":
        if opnieuw == "no":
         speel = False
         break
-       else:
-         speel = True
+       if opnieuw == "yes":
+        speel = False
+        break
      print 
      poging = input ('Guess the letter:')
 
@@ -76,19 +77,111 @@ if inp == "Start":
           break
      
 elif inp == "Instructions":
-  print("Before the game starts, you can choose the options of the game. You can choose the length of the word you have to guess. Once you presses play, you have 10 tries to guess the right word, by choosing diffrent types of letters. If the letter is found in the word, it will be put on the dots. If the letter is not in the word, than the start of a picture will be drawn. If the picture is completed before you have finished the word than you lose. Have you guessed the word, than you win.")
-else:
-    print("Invalid input!")
-
-print ("Is that clear?")
+ print("Before the game starts, you can choose the options of the game. You can choose the length of the word you have to guess. Once you presses play, you have 10 tries to guess the right word, by choosing diffrent types of letters. If the letter is found in the word, it will be put on the dots. If the letter is not in the word, than the start of a picture will be drawn. If the picture is completed before you have finished the word than you lose. Have you guessed the word, than you win. Is that clear?") 
 antwoord = input("yes / no")
 if antwoord == "yes":
  print("Let's start the game then!")
+ speel = True 
+ while speel:
+   woordkeuze = random.choice(woorden) 
+   woord = (woordkeuze)
+   pogingen =' '
+   beurten = 10
+   while beurten > 0:
+     lettersover = 0
+     geradenwoord =' '
+     for char in woord:
+       if char in pogingen:
+         geradenwoord=geradenwoord + (char)
+       else:
+         geradenwoord=geradenwoord+ ('_')
+         lettersover += 1
+     print ('Word:',geradenwoord, '.Letters that have already been guessed:', pogingen)                             
+     if lettersover == 0:
+       print("You have won!")
+       opnieuw=str(input("Play again? Type yes or no"))
+       if opnieuw == "no":
+        speel = False
+        break
+       if opnieuw == "yes":
+        speel = False
+        break
+     print 
+     poging = input ('Guess the letter:')
+
+     pogingen += poging
+     if poging not in woord:
+       beurten -= 1
+      
+       galglayout = beurten
+       levens=(10-beurten)
+       while levens>0:
+         print (galg[galglayout])
+         galglayout +=1
+         levens -=1
+       print ("")
+
+       print ("Unfortunately, that is wrong!")
+       print ('You still have', + beurten, 'lives')
+       if beurten == 0:
+         print ('Too bad!', usertext, 'You have lost!, the word was:', woord)
+         opnieuw=str(input("Play again? Type yes or no"))  
+         if opnieuw == "no":
+          speel = False
+          break
   
 elif antwoord == "no":
   print("Here are the instructions again")
   print("Before the game starts, you can choose the options of the game. You can choose the length of the word you have to guess. Once you presses play, you have 8 tries to guess the right word, by choosing diffrent types of letters. If the letter is found in the word, it will be put on the dots. If the letter is not in the word, than the start of a picture will be drawn. If the picture is completed before you have finished the word than you lose. Have you guessed the word, than you win.")
   print ("Now, let's start the game!")
+  speel = True 
+  while speel:
+   woordkeuze = random.choice(woorden) 
+   woord = (woordkeuze)
+   pogingen =' '
+   beurten = 10
+   while beurten > 0:
+     lettersover = 0
+     geradenwoord =' '
+     for char in woord:
+       if char in pogingen:
+         geradenwoord=geradenwoord + (char)
+       else:
+         geradenwoord=geradenwoord+ ('_')
+         lettersover += 1
+     print ('Word:',geradenwoord, '.Letters that have already been guessed:', pogingen)                             
+     if lettersover == 0:
+       print("You have won!")
+       opnieuw=str(input("Play again? Type yes or no"))
+       if opnieuw == "no":
+        speel = False
+        break
+       if opnieuw == "yes":
+        speel = False
+        break
+     print 
+     poging = input ('Guess the letter:')
+
+     pogingen += poging
+     if poging not in woord:
+       beurten -= 1
+      
+       galglayout = beurten
+       levens=(10-beurten)
+       while levens>0:
+         print (galg[galglayout])
+         galglayout +=1
+         levens -=1
+       print ("")
+
+       print ("Unfortunately, that is wrong!")
+       print ('You still have', + beurten, 'lives')
+       if beurten == 0:
+         print ('Too bad!', usertext, 'You have lost!, the word was:', woord)
+         opnieuw=str(input("Play again? Type yes or no"))  
+         if opnieuw == "no":
+          speel = False
+          break
 
  
 
